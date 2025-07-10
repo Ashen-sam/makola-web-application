8905f0b06393dbde22bffcb5632fb1526a2f7216
Ashen-sam <iamashen27@gmail.com>
2025-07-10 07:22:24 +0530

    feat : admin frontend layout creation and modifcations

---
+++ app/admin/issues/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import AdminIssues from "../../../pages/admin-issues"
+
+export default function AdminIssuesPage() {
+  return <AdminIssues />
+}
---
+++ app/admin/users/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import AdminUsers from "../../../pages/admin-users"
+
+export default function AdminUsersPage() {
+  return <AdminUsers />
+}
